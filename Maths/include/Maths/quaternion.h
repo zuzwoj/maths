fa07c5535a8172571e0d9a406747791423602647
Zuzanna Wójtowicz <zuzannawojtowicz42@gmail.com>
2025-08-10 10:16:04 +0200

    add public getters for quaternion components, rename t component to w

--- Maths/include/Maths/quaternion.h
+++ Maths/include/Maths/quaternion.h
@@ -5,17 +5,23 @@
 class Quaternion
 {
 private:
-	double x, y, z, t;
+	double w, x, y, z;
 
 	double len() const;
 	void normalize();
 
 public:
 	Quaternion(Vector3 rotationAxis, float rotationAngle);
-	Quaternion(Vector4 xyzt);
+	Quaternion(Vector4 wxyz);
+
+	double getW() { return w; }
+	double getX() { return x; }
+	double getY() { return y; }
+	double getZ() { return z; }
 
 	Matrix4x4 getRotationMatrix();
 	void reset();
+
 
 	Quaternion& operator*=(const Quaternion& rhs);
 	friend Quaternion operator*(Quaternion lhs, const Quaternion& rhs);
--- Maths/src/quaternion.cpp
+++ Maths/src/quaternion.cpp
@@ -2,7 +2,7 @@
 
 double Quaternion::len() const
 {
-	return sqrt(x * x + y * y + z * z + t * t);
+	return sqrt(x * x + y * y + z * z + w * w);
 }
 
 void Quaternion::normalize()
@@ -10,45 +10,45 @@ void Quaternion::normalize()
 	double l = len();
 	if (l != 0)
 	{
+		w /= l;
 		x /= l;
 		y /= l;
 		z /= l;
-		t /= l;
 	}
 }
 
 Quaternion::Quaternion(Vector3 rotationAxis, float rotationAngle)
 {
+	this->w = cos(rotationAngle / 2);
 	this->x = rotationAxis.x * sin(rotationAngle / 2);
 	this->y = rotationAxis.y * sin(rotationAngle / 2);
 	this->z = rotationAxis.z * sin(rotationAngle / 2);
-	this->t = cos(rotationAngle / 2);
 }
 
 Quaternion::Quaternion(Vector4 xyzt)
 {
+	this->w = xyzt.t;
 	this->x = xyzt.x;
 	this->y = xyzt.y;
 	this->z = xyzt.z;
-	this->t = xyzt.t;
 }
 
 Matrix4x4 Quaternion::getRotationMatrix()
 {
 	return Matrix4x4(
-		1.0f - 2.0f * (y * y + z * z), 2.0f * (x * y - z * t), 2.0f * (x * z + y * t), 0.0f,
-		2.0f * (x * y + z * t), 1.0f - 2.0f * (x * x + z * z), 2.0f * (y * z - x * t), 0.0f,
-		2.0f * (x * z - y * t), 2.0f * (y * z + x * t), 1.0f - 2.0f * (x * x + y * y), 0.0f,
+		1.0f - 2.0f * (y * y + z * z), 2.0f * (x * y - z * w), 2.0f * (x * z + y * w), 0.0f,
+		2.0f * (x * y + z * w), 1.0f - 2.0f * (x * x + z * z), 2.0f * (y * z - x * w), 0.0f,
+		2.0f * (x * z - y * w), 2.0f * (y * z + x * w), 1.0f - 2.0f * (x * x + y * y), 0.0f,
 		0.0f, 0.0f, 0.0f, 1.0f
 	);
 }
 
 void Quaternion::reset()
 {
+	this->w = 1.0f;
 	this->x = 0.0f;
 	this->y = 0.0f;
 	this->z = 0.0f;
-	this->t = 1.0f;
 }
 
 Quaternion& Quaternion::operator*=(const Quaternion& rhs)
@@ -60,10 +60,10 @@ Quaternion& Quaternion::operator*=(const Quaternion& rhs)
 Quaternion operator*(Quaternion lhs, const Quaternion& rhs)
 {
 	Quaternion result = Quaternion(Vector4(
-		lhs.t * rhs.x + lhs.x * rhs.t + lhs.y * rhs.z - lhs.z * rhs.y,
-		lhs.t * rhs.y - lhs.x * rhs.z + lhs.y * rhs.t + lhs.z * rhs.x,
-		lhs.t * rhs.z + lhs.x * rhs.y - lhs.y * rhs.x + lhs.z * rhs.t,
-		lhs.t * rhs.t - lhs.x * rhs.x - lhs.y * rhs.y - lhs.z * rhs.z
+		lhs.w * rhs.x + lhs.x * rhs.w + lhs.y * rhs.z - lhs.z * rhs.y,
+		lhs.w * rhs.y - lhs.x * rhs.z + lhs.y * rhs.w + lhs.z * rhs.x,
+		lhs.w * rhs.z + lhs.x * rhs.y - lhs.y * rhs.x + lhs.z * rhs.w,
+		lhs.w * rhs.w - lhs.x * rhs.x - lhs.y * rhs.y - lhs.z * rhs.z
 	));
 	result.normalize();
 	return result;
